b4877dbba4c81128845dc9ede625d5a27c0dcee5
Edilson Jesus <edilsondevelop@gmail.com>
2022-04-07 22:08:24 -0300

    Iniciando capitulo 3

--- index.js
+++ index.js
@@ -20,20 +20,46 @@ cliente2.cpf = 98765432100;
 class ContaCorrente {
     agencia;
     saldo;
+
+    sacar(valor){
+        if(this.saldo >= valor){
+            this.saldo -= valor
+        }
+    }
+
 }
 
 const contaCorrentePedro = new ContaCorrente ();
 contaCorrentePedro.saldo = 0;
 contaCorrentePedro.agencia = 1001;
 
-console.log(contaCorrentePedro.saldo);
-contaCorrentePedro.saldo = 100;
-console.log(contaCorrentePedro.saldo);
-let valorSacado = 200;
-contaCorrentePedro.saldo -= valorSacado; 
+
+
+// TEMOS UMA OPERAÇÃO QUE NÃO ESTÁ PROTEGENDO A REGRA DE NEGOCIO DO BANCO
+// console.log(contaCorrentePedro.saldo);
+// contaCorrentePedro.saldo = 100;
+// console.log(contaCorrentePedro.saldo);
+// let valorSacado = 200;
+// contaCorrentePedro.saldo -= valorSacado; 
+
+// console.log(contaCorrentePedro.saldo);
+
+// VAMOS USAR A MESMA ESTRUTURA, POREM MODIFICANDO E ACRESCENTANDO UMA VERIFICAÇÃO 
+// console.log(contaCorrentePedro.saldo);
+// contaCorrentePedro.saldo = 100;
+// console.log(contaCorrentePedro.saldo);
+// let valorSacado = 200;
+// if (contaCorrentePedro.saldo >= valorSacado){
+//     contaCorrentePedro.saldo -= valor;
+//     };
+// Agora vamos costumizar e colocar em uma função    
 
 console.log(contaCorrentePedro.saldo);
+contaCorrentePedro.saldo = 100; 
+console.log(contaCorrentePedro.saldo); 
+contaCorrentePedro.sacar(200);
 
+console.log(contaCorrentePedro.saldo);
 
 
 console.log(cliente1);
